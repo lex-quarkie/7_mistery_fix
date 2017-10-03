4439bad03ea7c9c28852fabdfd2c7d418c22badb
Lex Quarkie <lex.quarkie@gmail.com>
2017-10-03 10:55:50 +0300

    fix falling test on negative discriminant

--- quadratic_equation.py
+++ quadratic_equation.py
@@ -3,9 +3,13 @@ from math import sqrt
 
 def get_roots(a, b, c):
     discriminant = b ** 2 - 4 * a * c
-    root1 = (-b - sqrt(discriminant)) / (2 * a)
-    root2 = (-b + sqrt(discriminant)) / (2 * a)
+    try:
+        root1 = (-b - sqrt(discriminant)) / (2 * a)
+        root2 = (-b + sqrt(discriminant)) / (2 * a)
+    except ValueError:
+        return None,None
     if discriminant == 0:
         return root1, None
     else:
         return root1, root2
+
